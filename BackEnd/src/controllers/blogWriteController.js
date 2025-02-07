258b7e6b31f08f40bf47790d8c1b17c9fdd1741a
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-02-07 15:51:49 +1100

    Add Routes for managing Blog Posts (CRUD)

--- BackEnd/src/controllers/blogWriteController.js
+++ BackEnd/src/controllers/blogWriteController.js
@@ -1,4 +1,61 @@
 async function blogWriteControllerMain(req, res, next) {
   res.json({ Welcome: "Write Route!" });
 }
-export { blogWriteControllerMain };
+
+async function blogWriteControllerDraft(req, res, next) {
+  try {
+    res.json({ Route: "Draft Route" });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function blogWriteControllerSave(req, res, next) {
+  try {
+    res.json({ Route: "Save Route" });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function blogWriteControllerEdit(req, res, next) {
+  try {
+    res.json({ Route: "Edit Route" });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function blogWriteControllerDelete(req, res, next) {
+  try {
+    res.json({ Route: "Delete Route" });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function blogWriteControllerPublish(req, res, next) {
+  try {
+    res.json({ Route: "Publish Route" });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function blogWriteControllerUnpublish(req, res, next) {
+  try {
+    res.json({ Route: "Unpublish Route" });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export {
+  blogWriteControllerMain,
+  blogWriteControllerDraft,
+  blogWriteControllerSave,
+  blogWriteControllerEdit,
+  blogWriteControllerDelete,
+  blogWriteControllerPublish,
+  blogWriteControllerUnpublish,
+};
--- BackEnd/src/routes/blogWriterRouter.js
+++ BackEnd/src/routes/blogWriterRouter.js
@@ -1,9 +1,29 @@
 import { Router } from "express";
 
-import { blogWriteControllerMain } from "../controllers/blogWriteController.js";
+import {
+  blogWriteControllerMain,
+  blogWriteControllerDraft,
+  blogWriteControllerSave,
+  blogWriteControllerEdit,
+  blogWriteControllerDelete,
+  blogWriteControllerPublish,
+  blogWriteControllerUnpublish,
+} from "../controllers/blogWriteController.js";
 
 const blogWriterRouter = Router();
 
 blogWriterRouter.get("/", blogWriteControllerMain);
+
+blogWriterRouter.post("/post/:id/draft", blogWriteControllerDraft);
+
+blogWriterRouter.post("/post/:id/save", blogWriteControllerSave);
+
+blogWriterRouter.put("/post/:id/edit", blogWriteControllerEdit);
+
+blogWriterRouter.delete("/post/:id/delete", blogWriteControllerDelete);
+
+blogWriterRouter.post("/post/:id/publish", blogWriteControllerPublish);
+
+blogWriterRouter.post("/post/:id/unpublish", blogWriteControllerUnpublish);
 
 export default blogWriterRouter;
