512d0dc68f82a59eaa93fc73c74fc9353a539c0a
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-02-17 11:08:19 +1100

    Add login form to main page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Blog API Front End</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <form>
+          <label id="username">User Name </label>
+          <br></br>
+          <input name="username" id="username" type="email"></input>
+          <br></br>
+          <label id="password">Password </label> <br></br>
+          <input name="password" id="password" type="password"></input>
+          <br></br>
+          <button>Login</button>
+          <button>Register</button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
