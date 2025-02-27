725ac8b1ca10f9cd155693f9d31f0d2fb03873e9
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-02-28 09:35:00 +1100

    Add userEffect and setState to component for Async Add userEffect and setState to component for Async function to fetch all posts

--- frontend/src/components/AllPosts.jsx
+++ frontend/src/components/AllPosts.jsx
@@ -1,13 +1,40 @@
-import {getAllPosts} from '../utils/apiReaderQueries'
-function AllPosts()
-{
-    return(
-        <div>
-            <h1>All Posts</h1>
-            
+import { useState, useEffect } from "react";
+import { getAllPosts } from "../utils/apiReaderQueries";
+function AllPosts() {
+  const [allThePosts, setAllThePosts] = useState([]);
+  const [error, setError] = useState(null);
 
-        </div>
-    )
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const fetchPosts = await getAllPosts();
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (allThePosts.length === 0) {
+    return (
+      <div>
+        <h3>Loading....</h3>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>All Posts</h1>
+      {allThePosts.map((posts) => (
+        <div key={posts.id}>{posts.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default AllPosts;
--- frontend/src/utils/apiReaderQueries.js
+++ frontend/src/utils/apiReaderQueries.js
@@ -1,13 +1,14 @@
 const apiURL = "http://localhost:3000";
 async function getAllPosts() {
   try {
-    let response = await fetch(`${apiURL}/reader`);
+    let response = await fetch(`${apiURL}/reader/posts`);
 
     if (!response.ok) {
       throw new Error(`HTTP Error! status:${response.status}`);
     }
 
     const allPosts = await response.json();
+    console.log(allPosts);
     return allPosts;
   } catch (error) {
     console.error("Error catching posts:", error);
