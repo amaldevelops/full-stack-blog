d1c695ccfe18e3a3594fd754c28a19176ef5cacb
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-03-05 12:35:41 +1100

    Refactored CSS into index.css and removed App.css

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import TechStack from "../components/TechStack";
-import "../assets/App.css";
+// import "../assets/App.css";
 
 import Login from "./Login";
 import AllPosts from "./AllPosts";
@@ -10,7 +10,7 @@ import AllPosts from "./AllPosts";
 function App() {
   return (
     <>
-    <Link to="reader/home">All Posts</Link>
+      <Link to="reader/home">All Posts</Link>
       <TechStack />
 
       <Login />
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,10 @@
 import AllPosts from "./AllPosts";
+import {Link} from "react-router-dom"
 function Home() {
   return (
     <div>
       <h1>Blog DashBoard</h1>
+      <Link>Write A new Post</Link>
       <AllPosts />
     </div>
   );
