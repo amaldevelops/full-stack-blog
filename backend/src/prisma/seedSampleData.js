b3023ddb0aee2208908e0253a7c7b1d52f9ab148
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-03-19 15:19:46 +1100

    Updated Sample seed data

--- backend/src/prisma/seedSampleData.js
+++ backend/src/prisma/seedSampleData.js
@@ -56,6 +56,26 @@ async function main() {
     },
   });
 
+  const post3 = await prisma.blogContent.create({
+    data: {
+      blog_post_title: "NodeJs Basics",
+      blog_post_content:
+        "Node.js is an open-source, cross-platform JavaScript runtime environment designed for building scalable network applications. Here are key aspects of Node.js: Features 1) Asynchronous and event-driven: Handles multiple operations concurrently without blocking. 2) Single-threaded: Uses an event loop to manage requests efficiently. 3)V8 engine: Runs on Google Chrome's V8 JavaScript engine for high performance. 4)NPM (Node Package Manager): Provides access to a vast ecosystem of open-source libraries.        Advantages 1)High performance: Non-blocking I/O and V8 engine optimization. 2) Full-stack JavaScript: Allows using JavaScript for both client and server-side development. 3)Scalability: Efficiently handles concurrent connections. 4) Cross-platform compatibility: Runs on various operating systems. 5) Large and active community: Provides continuous support and updates",
+      blog_post_publish_status: true,
+      blog_post_author_id: user2.id,
+    },
+  });
+
+  const post4 = await prisma.blogContent.create({
+    data: {
+      blog_post_title: "React Basics",
+      blog_post_content:
+        "React is a popular JavaScript library for building user interfaces, developed by Facebook and released in 2013. It has become a cornerstone of modern web development due to its efficiency and flexibility.       1) Component-Based Architecture: React uses reusable components as building blocks for UIs, promoting modularity and maintainability 2) Virtual DOM: React employs a lightweight in-memory representation of the actual DOM, optimizing rendering performance. 3) JSX: A syntax extension allowing HTML-like code within JavaScript, simplifying component creation.4) Unidirectional Data Flow: Ensures predictable behavior and clear data hierarchy. 5)React Hooks: Enables state management and other functionalities in function components",
+      blog_post_publish_status: true,
+      blog_post_author_id: user2.id,
+    },
+  });
+
   // Seed comments
   await prisma.blogComments.createMany({
     data: [
@@ -68,6 +88,36 @@ async function main() {
         comment_text: "I love using async/await in JavaScript.",
         blog_post_id: post2.id,
         comment_author_id: user2.id,
+      },
+      {
+        comment_text: "Well Written Article",
+        blog_post_id: post2.id,
+        comment_author_id: user2.id,
+      },
+      {
+        comment_text: "I learned a lot",
+        blog_post_id: post2.id,
+        comment_author_id: user1.id,
+      },
+      {
+        comment_text: "Well Written Article",
+        blog_post_id: post3.id,
+        comment_author_id: user2.id,
+      },
+      {
+        comment_text: "I learned a lot",
+        blog_post_id: post3.id,
+        comment_author_id: user1.id,
+      },
+      {
+        comment_text: "Well Written Article",
+        blog_post_id: post4.id,
+        comment_author_id: user2.id,
+      },
+      {
+        comment_text: "I learned a lot",
+        blog_post_id: post4.id,
+        comment_author_id: user1.id,
       },
     ],
   });
