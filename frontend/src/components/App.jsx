191fe407c9a0972566fe3c6ef1404d4550e39706
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-04-21 17:45:14 +1000

    Moved documentation to Documentation.jsx component

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -11,104 +11,12 @@ import TechStack from "../components/TechStack";
 import Documentation from "./Documentation";
 import UserDetails from "./UserDetails";
 
-import logoReact from "../assets/react.svg";
-import logoJs from "../assets/logo-js.svg";
-import logoCSS from "../assets/logo-css.svg";
-import logoHtml from "../assets/logo-html.svg";
-import logoNodejs from "../assets/logo-nodejs.svg";
-import logoPostgresql from "../assets/logo-postgressql.svg";
-import logoPostman from "../assets/logo-postman.svg";
-import logoPrisma from "../assets/logo-prisma.svg";
-import logoVite from "../assets/logo-vite.svg";
-
 function App() {
   return (
     <>
       {/* <AllPosts /> */}
       <UserDetails />
-      <div>
-        <h1>Introduction</h1>
-        <p>
-          This demo project is designed to strengthen the technical proficiency
-          in building a RESTful API with a strong emphasis on authentication,
-          route protection, and role-based access control using JSON Web Tokens
-          (JWTs). The core objective is to implement a secure backend that can
-          effectively differentiate between various types of users—such as
-          readers and writers—while enforcing access restrictions accordingly.{" "}
-        </p>
-      </div>
 
-      <div className="card">
-        <div>
-          <div>
-            <h2>Frontend </h2>
-            <img src={logoJs} className="Logos" alt="JavaScript Logo" />
-            <img src={logoReact} className="Logos" alt="React logo" />
-            <img src={logoCSS} className="Logos" alt="CSS logo" />
-            <img src={logoHtml} className="Logos" alt="HTML logo" />
-            <img src={logoVite} className="Logos" alt="Vite logo" />
-            <h3>Blog Reader</h3>
-            <ul>
-              <li>JavaScript,React, Vite HTML, CSS</li>
-              <li>
-                React-based front-end for reading blog posts and leaving
-                comments.
-              </li>
-              <li>
-                User-friendly interface for browsing and interacting with
-                content.
-              </li>
-              <li>
-                Reader Client: Provides a user-friendly interface for consuming
-                blog content.
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3>Blog Writer</h3>
-            <ul>
-              <li>JavaScript,React, Vite HTML, CSS</li>
-              <li>
-                React-based front-end for reading blog posts and leaving
-                comments.
-              </li>
-              <li>
-                User-friendly interface for browsing and interacting with
-                content.
-              </li>
-
-              <li>
-                Writer Client: Features for managing posts
-                (publishing/unpublishing, editing), comments (deleting,
-                editing). Offers a dedicated interface for authors to manage
-                blog content and administrative tasks.
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div>
-          <h2>Back End </h2>
-          <img src={logoNodejs} className="Logos" alt="NodeJs logo" />
-          <img src={logoPostgresql} className="Logos" alt="PostgreSQL logo" />
-          <img src={logoPrisma} className="Logos" alt="Prisma logo" />
-          <img src={logoPostman} className="Logos" alt="Postman logo" />
-
-          <h3>RESTful API </h3>
-          <ul>
-            <li>RESTful API built with Node.js, Express and PostgreSQL.</li>
-            <li>Prisma ORM for database interaction.</li>
-            <li>JWT authentication for secure access to protected routes.</li>
-            <li>
-              Endpoints for managing blog posts, comments, and user accounts.
-            </li>
-            <li>
-              The API serves as the central data and logic hub, handling
-              requests from both front-end clients.
-            </li>
-          </ul>
-        </div>
-      </div>
       {/* <TechStack /> */}
       <Documentation />
     </>
--- frontend/src/components/Documentation.jsx
+++ frontend/src/components/Documentation.jsx
@@ -1,17 +1,141 @@
 // import NavigationBar from "./NavigationBar";
+import logoReact from "../assets/react.svg";
+import logoJs from "../assets/logo-js.svg";
+import logoCSS from "../assets/logo-css.svg";
+import logoHtml from "../assets/logo-html.svg";
+import logoNodejs from "../assets/logo-nodejs.svg";
+import logoPostgresql from "../assets/logo-postgressql.svg";
+import logoPostman from "../assets/logo-postman.svg";
+import logoPrisma from "../assets/logo-prisma.svg";
+import logoVite from "../assets/logo-vite.svg";
 function APIDocs() {
   return (
     <div>
-      {/* <NavigationBar /> */}
-      <h1>Documentation for Blog API & Blog Reader / Writer Front End</h1>
-      <h2>Deploying to a PAAS</h2>
-      <ul>
-        <li>Set up your Prisma schema: npx prisma generate
+      <div>
+        <h1>Introduction</h1>
+        <p>
+          This demo project is designed to strengthen the technical proficiency
+          in building a RESTful API with a strong emphasis on authentication,
+          route protection, and role-based access control using JSON Web Tokens
+          (JWTs). The core objective is to implement a secure backend that can
+          effectively differentiate between various types of users—such as
+          readers and writers—while enforcing access restrictions accordingly.{" "}
+        </p>
+      </div>
+      <div>
+        <h2>Live Demo</h2>
+        <ul>
+          <li>
+            Front End Hosted on GitHub Pages:{" "}
+            <a href="https://www.amalk.au/full-stack-blog" target="_blank">
+              https://www.amalk.au/full-stack-blog
+            </a>
+          </li>
+          <li>
+            Database hosted on Neon:
+            <a href="https://neon.tech/" target="_blank">
+              https://neon.tech/
+            </a>
+          </li>
+          <li>
+            Backend hosted on Render:{" "}
+            <a href="https://full-stack-blog-t1n4.onrender.com" target="_blank">
+              https://full-stack-blog-t1n4.onrender.com
+            </a>{" "}
+          </li>
+        </ul>
+      </div>
 
+      <div className="card">
+        <div>
+          <div>
+            <h2>Frontend </h2>
+            <img src={logoJs} className="Logos" alt="JavaScript Logo" />
+            <img src={logoReact} className="Logos" alt="React logo" />
+            <img src={logoCSS} className="Logos" alt="CSS logo" />
+            <img src={logoHtml} className="Logos" alt="HTML logo" />
+            <img src={logoVite} className="Logos" alt="Vite logo" />
+            <h3>Blog Reader</h3>
+            <ul>
+              <li>JavaScript,React, Vite HTML, CSS</li>
+              <li>
+                React-based front-end for reading blog posts and leaving
+                comments.
+              </li>
+              <li>
+                User-friendly interface for browsing and interacting with
+                content.
+              </li>
+              <li>
+                Reader Client: Provides a user-friendly interface for consuming
+                blog content.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Blog Writer</h3>
+            <ul>
+              <li>JavaScript,React, Vite HTML, CSS</li>
+              <li>
+                React-based front-end for reading blog posts and leaving
+                comments.
+              </li>
+              <li>
+                User-friendly interface for browsing and interacting with
+                content.
+              </li>
+
+              <li>
+                Writer Client: Features for managing posts
+                (publishing/unpublishing, editing), comments (deleting,
+                editing). Offers a dedicated interface for authors to manage
+                blog content and administrative tasks.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <h2>Back End </h2>
+          <img src={logoNodejs} className="Logos" alt="NodeJs logo" />
+          <img src={logoPostgresql} className="Logos" alt="PostgreSQL logo" />
+          <img src={logoPrisma} className="Logos" alt="Prisma logo" />
+          <img src={logoPostman} className="Logos" alt="Postman logo" />
+
+          <h3>RESTful API </h3>
+          <ul>
+            <li>RESTful API built with Node.js, Express and PostgreSQL.</li>
+            <li>Prisma ORM for database interaction.</li>
+            <li>JWT authentication for secure access to protected routes.</li>
+            <li>
+              Endpoints for managing blog posts, comments, and user accounts.
+            </li>
+            <li>
+              The API serves as the central data and logic hub, handling
+              requests from both front-end clients.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <h2>How to deploy to a PAAS</h2>
+      <ul>
+        <li>Set up your Prisma schema: npx prisma generate</li>
+        <li>Push schema to Neon: npx prisma db push</li>
+        <li>
+          Seed sample data (This is saved in the package.json) : npm run seed
         </li>
-        <li>Push schema to Neon: npx prisma db push
+        <li>
+          Environment Variable setup
+          <ul>
+            <li>CORS_URL:"https://www.amalk.au/"</li>
+            <li>DATABASE_URL: Input the database URL</li>
+            <li>PORT : This is the Port that the program will run on</li>
+            <li>
+              JWT_SECRET_KEY: This is the custom secret key used to encrypt JWT
+            </li>
+          </ul>
         </li>
-        <li>Seed sample data (This is saved in the package.json) : npm run seed</li>
       </ul>
       <h2>Blog API</h2>
       <p>
